8a6b4b8f0644c938ebc31b38334362f56c838980
Fernando <fmateora@everis.com>
2018-07-30 17:50:27 -0500

    Creación del proyecto

---
+++ terapeutas/terapeutas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  terapeutas
+//
+//  Created by Fernando Mateo on 7/30/18.
+//  Copyright © 2018 Fernando Mateo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ terapeutas/terapeutas/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  terapeutas
+//
+//  Created by Fernando Mateo on 7/30/18.
+//  Copyright © 2018 Fernando Mateo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
